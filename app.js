326e02e606bc8fbdc80dbbb28b25628dacd38780
Victor Salako <v.salako@aol.co.uk>
2023-11-04 12:21:17 +0000

    Updated code following .gitignore

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/user-routes';
 import blogRouter from './routes/blog-routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 6000;
@@ -11,9 +14,7 @@ app.use('/api/user', router);
 app.use('/api/blog', blogRouter);
 
 mongoose
-  .connect(
-    'mongodb+srv://admin:si4Ql6m0jr1yBe4J@blogappcluster.bi3xgfa.mongodb.net/BlogApp?retryWrites=true&w=majority'
-  )
+  .connect(`${process.env.MONGODB_CLUSTER}`)
   .then(() =>
     app.listen(port, () =>
       console.log(
